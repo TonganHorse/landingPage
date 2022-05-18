a1b8db9575ee1d82a32bcf650ab39b46c32eabb2
tuli kinikini <tuli.kinikini@gmail.com>
2022-05-18 14:34:34 -0600

    added file path

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const path = require('path')
 const app = express()
 
 
-app.get(path.join(__dirname, "../public/index.html"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+})
 
 
 
