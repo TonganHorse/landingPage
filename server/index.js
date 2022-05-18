e2ce115f87ca82b22f641c180e2f9f3f9e21d2f6
tuli kinikini <tuli.kinikini@gmail.com>
2022-05-18 14:42:22 -0600

    linked files

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
+app.get("/styles", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.css"))
+})
 
+app.get("/js", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.js"))
+})
 
 port = process.env.PORT || 4005 
 
