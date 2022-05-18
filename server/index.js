ddb764fe289f7157171127506774f84e6eea3a9b
tuli kinikini <tuli.kinikini@gmail.com>
2022-05-18 15:21:34 -0600

    trying express static

--- server/index.js
+++ server/index.js
@@ -3,17 +3,20 @@ const path = require('path')
 const app = express()
 
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
-})
 
-app.get("/styles", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.css"))
-})
+app.use("/", express.static(path.join(__dirname, "../public")));
 
-app.get("/js", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/main.js"))
-})
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html"))
+// })
+
+// app.get("/styles", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.css"))
+// })
+
+// app.get("/js", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/main.js"))
+// })
 
 port = process.env.PORT || 4005 
 
